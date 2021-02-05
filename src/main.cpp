ba63cac350627278df33fcfe13f7582cd1549223
KacperKostka08 <32651459+kacperks@users.noreply.github.com>
2021-02-06 00:11:50 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,18 @@
-#include <iostream>
-#include <vector>
-#include <utility>
-#include <algorithm>
-#include <chrono>
+#include "core.hpp"
+
 using namespace std;
 
-#include <stdio.h>
-#include <Windows.h>
-
-int nScreenWidth = 120;			// Console Screen Size X (columns)
-int nScreenHeight = 40;			// Console Screen Size Y (rows)
-int nMapWidth = 16;				// World Dimensions
+int nScreenWidth = 120;			
+int nScreenHeight = 40;			
+int nMapWidth = 16;				
 int nMapHeight = 16;
 
-float fPlayerX = 14.7f;			// Player Start Position
+float fPlayerX = 14.7f;			
 float fPlayerY = 5.09f;
-float fPlayerA = 0.0f;			// Player Start Rotation
-float fFOV = 3.14159f / 4.0f;	// Field of View
-float fDepth = 16.0f;			// Maximum rendering distance
-float fSpeed = 5.0f;			// Walking Speed
+float fPlayerA = 0.0f;			
+float fFOV = 3.14159f / 4.0f;	
+float fDepth = 16.0f;			
+float fSpeed = 5.0f;			
 
 int main()
 {
@@ -52,24 +46,21 @@ int main()
 	
 	while (1)
 	{
-		// We'll need time differential per frame to calculate modification
-		// to movement speeds, to ensure consistant movement, as ray-tracing
-		// is non-deterministic
 		tp2 = chrono::system_clock::now();
 		chrono::duration<float> elapsedTime = tp2 - tp1;
 		tp1 = tp2;
 		float fElapsedTime = elapsedTime.count();
 
 
-		// Handle CCW Rotation
+		
 		if (GetAsyncKeyState((unsigned short)'A') & 0x8000)
 			fPlayerA -= (fSpeed * 0.75f) * fElapsedTime;
 
-		// Handle CW Rotation
+		
 		if (GetAsyncKeyState((unsigned short)'D') & 0x8000)
 			fPlayerA += (fSpeed * 0.75f) * fElapsedTime;
 		
-		// Handle Forwards movement & collision
+		
 		if (GetAsyncKeyState((unsigned short)'W') & 0x8000)
 		{
 			fPlayerX += sinf(fPlayerA) * fSpeed * fElapsedTime;;
@@ -81,7 +72,7 @@ int main()
 			}			
 		}
 
-		// Handle backwards movement & collision
+		
 		if (GetAsyncKeyState((unsigned short)'S') & 0x8000)
 		{
 			fPlayerX -= sinf(fPlayerA) * fSpeed * fElapsedTime;;
@@ -95,21 +86,20 @@ int main()
 
 		for (int x = 0; x < nScreenWidth; x++)
 		{
-			// For each column, calculate the projected ray angle into world space
+			
 			float fRayAngle = (fPlayerA - fFOV/2.0f) + ((float)x / (float)nScreenWidth) * fFOV;
 
-			// Find distance to wall
-			float fStepSize = 0.1f;		  // Increment size for ray casting, decrease to increase										
-			float fDistanceToWall = 0.0f; //                                      resolution
+			
+			float fStepSize = 0.1f;		  										
+			float fDistanceToWall = 0.0f; 
 
-			bool bHitWall = false;		// Set when ray hits wall block
-			bool bBoundary = false;		// Set when ray hits boundary between two wall blocks
+			bool bHitWall = false;		
+			bool bBoundary = false;		
 
-			float fEyeX = sinf(fRayAngle); // Unit vector for ray in player space
+			float fEyeX = sinf(fRayAngle);
 			float fEyeY = cosf(fRayAngle);
 
-			// Incrementally cast ray from player, along ray angle, testing for 
-			// intersection with a block
+			
 			while (!bHitWall && fDistanceToWall < fDepth)
 			{
 				fDistanceToWall += fStepSize;
@@ -119,25 +109,21 @@ int main()
 				// Test if ray is out of bounds
 				if (nTestX < 0 || nTestX >= nMapWidth || nTestY < 0 || nTestY >= nMapHeight)
 				{
-					bHitWall = true;			// Just set distance to maximum depth
+					bHitWall = true;			
 					fDistanceToWall = fDepth;
 				}
 				else
 				{
-					// Ray is inbounds so test to see if the ray cell is a wall block
+					
 					if (map.c_str()[nTestX * nMapWidth + nTestY] == '#')
 					{
-						// Ray has hit wall
+						
 						bHitWall = true;
 
-						// To highlight tile boundaries, cast a ray from each corner
-						// of the tile, to the player. The more coincident this ray
-						// is to the rendering ray, the closer we are to a tile 
-						// boundary, which we'll shade to add detail to the walls
+						
 						vector<pair<float, float>> p;
 
-						// Test each corner of hit tile, storing the distance from
-						// the player, and the calculated dot product of the two rays
+						
 						for (int tx = 0; tx < 2; tx++)
 							for (int ty = 0; ty < 2; ty++)
 							{
@@ -149,7 +135,7 @@ int main()
 								p.push_back(make_pair(d, dot));
 							}
 
-						// Sort Pairs from closest to farthest
+						
 						sort(p.begin(), p.end(), [](const pair<float, float> &left, const pair<float, float> &right) {return left.first < right.first; });
 						
 						// First two/three are closest (we will never see all four)
@@ -161,11 +147,11 @@ int main()
 				}
 			}
 		
-			// Calculate distance to ceiling and floor
+			
 			int nCeiling = (float)(nScreenHeight/2.0) - nScreenHeight / ((float)fDistanceToWall);
 			int nFloor = nScreenHeight - nCeiling;
 
-			// Shader walls based on distance
+			
 			short nShade = ' ';
 			if (fDistanceToWall <= fDepth / 4.0f)			nShade = 0x2588;	// Very close	
 			else if (fDistanceToWall < fDepth / 3.0f)		nShade = 0x2593;
@@ -177,12 +163,12 @@ int main()
 			
 			for (int y = 0; y < nScreenHeight; y++)
 			{
-				// Each Row
+				
 				if(y <= nCeiling)
 					screen[y*nScreenWidth + x] = ' ';
 				else if(y > nCeiling && y <= nFloor)
 					screen[y*nScreenWidth + x] = nShade;
-				else // Floor
+				else 
 				{				
 					// Shade floor based on distance
 					float b = 1.0f - (((float)y -nScreenHeight/2.0f) / ((float)nScreenHeight / 2.0f));
